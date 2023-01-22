5256e0eb9c69eb33012f558ca57128aae0efacf6
jiyeong-dt <imjee02@gmail.com>
2023-01-23 00:05:02 +0900

    feat : Form login 인증

--- src/main/java/com/spring/security/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/spring/security/springsecurity/security/SecurityConfig.java
@@ -18,9 +18,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().authenticated();
 
-        //인증 정책
-        http
-                .formLogin();
+        loginApi(http);
+    }
 
+
+    //Form Login 인증 API
+    private void loginApi(HttpSecurity http) throws Exception {
+
+        http.formLogin()
+//                .loginPage("/loginPage") //사용자 정의 로그인 페이지
+                .defaultSuccessUrl("/") // 성공시 이동 페이지
+                .failureUrl("/login") //실패시 이동 페이지
+                .usernameParameter("userId") // id 파라미터명
+                .passwordParameter("password") // password 파라미터명
+                .loginProcessingUrl("/login_proc") // form 요청 url
+                .successHandler((request , response , authentication)-> {
+                        System.out.println("성공!");
+                        response.sendRedirect("/");
+                })
+                .failureHandler((request , response , exception)-> {
+                        System.out.println("실패");
+                        response.sendRedirect("/login");
+                })// 로그인 실패후 핸들러
+                .permitAll();  //로그인 페이지 permit 뚫어주기
     }
 }
