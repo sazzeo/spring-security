8ec3983e22148f580e2c23ab9f50d40006e02bac
jiyeong-dt <imjee02@gmail.com>
2023-02-20 22:55:27 +0900

    feat : chapter2/ 다중 Config 설정

---
+++ chapter2/src/main/java/com/example/chapter1/config/MultiConfig2.java
@@ -0,0 +1,22 @@
+package com.example.chapter1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@Order(0)
+public class MultiConfig2 extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin();
+    }
+}
