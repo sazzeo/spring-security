3608c0510ba42f8f03957169a92c1423a3f8d6ca
jiyeong-dt <imjee02@gmail.com>
2023-02-20 22:37:41 +0900

    feat : chapter1

--- chapter1/src/main/java/com/example/chapter1/config/AuthorizationConfig.java
+++ chapter1/src/main/java/com/example/chapter1/config/AuthorizationConfig.java
@@ -1,4 +1,4 @@
-package com.spring.security.springsecurity.security;
+package com.example.chapter1.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
--- chapter1/src/main/java/com/example/chapter1/controller/SecurityController.java
+++ chapter1/src/main/java/com/example/chapter1/controller/SecurityController.java
@@ -1,4 +1,4 @@
-package com.spring.security.springsecurity.admin;
+package com.example.chapter1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
